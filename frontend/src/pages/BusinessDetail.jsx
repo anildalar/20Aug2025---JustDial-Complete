e639d79647708fb1039d83a71b78761bc6172eba
anildalar <dollorinfotech@gmail.com>
2025-08-21 10:11:33 +0530

    new

--- frontend/src/pages/BusinessDetail.jsx
+++ frontend/src/pages/BusinessDetail.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
 import { useParams } from 'react-router-dom';
 import { BASE_URL } from '../helper/Helper';
 
 export default function BusinessDetail() {
     const [businessDetail,setBusinessDetail] = useState();
+    const [star,setStar] = useState(0);
+    const [reviewContent,setReviewContent] = useState('');
     console.log('businessDetail>>>>',businessDetail);
     const { b_detail_id } = useParams();
 
@@ -29,6 +32,60 @@ export default function BusinessDetail() {
         }
     },[]);
 
+    //2.2 FUnction Defination
+    let starCount = (starCountNumber)=>{
+        console.log(starCountNumber);
+        setStar(starCountNumber)
+
+    }
+    let handleChange = (e)=>{
+        console.log(e.target.value);
+        setReviewContent(e.target.value);
+    }
+    let sendReview = ()=>{
+        let payload = {
+                            "data": {
+                                "stars": `${star}`,
+                                "review_content": reviewContent,
+                                "business_details": [`${b_detail_id}`],
+                                "users_permissions_users": ["1"],
+                                "review_photos": [],
+                            },
+                        };
+        console.log(payload);
+
+        //Lets call the api
+        try {
+            axios.post(`${BASE_URL}/api/reviews`, payload,                     // request body
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+            .then(function (response) {
+                // handle success
+                console.log(response?.data?.data);
+                Swal.fire({
+                    title: "Review Submitted Successfully!",
+                    icon: "success",
+                    draggable: true
+                });
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    //2.3 Return statement
     return (
         <>
         
@@ -45,25 +102,25 @@ export default function BusinessDetail() {
             }
             <hr />
             <h1>Review </h1>
-            <ul class="list-inline">
-                <li class="list-inline-item">
-                    <a href="#star1"><i class="fas fa-star text-warning"></i></a>
+            <ul className="list-inline">
+                <li className="list-inline-item">
+                    <a href="#star5" onClick={()=>{ starCount(1) }}><i className="far fa-star text-warning"></i></a>
                 </li>
-                <li class="list-inline-item">
-                    <a href="#star2"><i class="fas fa-star text-warning"></i></a>
+                <li className="list-inline-item">
+                    <a href="#star5" onClick={()=>{ starCount(2) }}><i className="far fa-star text-warning"></i></a>
                 </li>
-                <li class="list-inline-item">
-                    <a href="#star3"><i class="fas fa-star text-warning"></i></a>
+                <li className="list-inline-item">
+                    <a href="#star5" onClick={()=>{ starCount(3) }}><i className="far fa-star text-warning"></i></a>
                 </li>
-                <li class="list-inline-item">
-                    <a href="#star4"><i class="fas fa-star text-warning"></i></a>
+                <li className="list-inline-item">
+                    <a href="#star5" onClick={()=>{ starCount(4) }}><i className="far fa-star text-warning"></i></a>
                 </li>
-                <li class="list-inline-item">
-                    <a href="#star5"><i class="far fa-star text-warning"></i></a>
+                <li className="list-inline-item">
+                    <a href="#star5" onClick={()=>{ starCount(5) }}><i className="far fa-star text-warning"></i></a>
                 </li>
             </ul>
-            <textarea className="form-control col-2"></textarea>
-            
+            <textarea className="form-control col-2" onChange={handleChange}></textarea>
+            <button onClick={sendReview} className="btn btn-success mt-3">Submit Review</button>
         </>
 
     )
